173fbd396f55c585c30ccffd955308aa4049c5db
rto07 <tar845@Rauls-MacBook-Pro.local>
2017-09-09 17:03:14 -0500

    created game logic

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log ("Up and Running!");
+
+var cardOne = "queen";
+
+var cardTwo = "king";
+
+var cardThree = "king";
+
+var cardFour = "queen";
+
+console.log ("User flipped" + cardOne);
+console.log ("User flipped" + cardThree);
